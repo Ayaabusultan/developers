ac6696e940694bd5d6638c0ff987521db16a9ba9
ohood84 <obeidat.ohood@gmail.com>
2019-04-01 19:27:51 -0700

    add scripts file

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+function Jobs{
+  this.jposts[];
+}
+
+Jobs.prototype.addJob=function(job){
+  this.jposts.push(job);
+
+}
+
+Jobs.prototype.removeJob=function(job){
+  this.jposts.pop(job);
+
+}
+
+
+  function JobPosting{
+  this.companyName;
+  this.title;
+  this.difficulty;
+  this.description;
+  this.skillsRequire=[];
+}
+
+function Developers{
+  this.developers[];
+}
+
+Developers.prototype.addDeveloper=function(developer){
+  this.developers.push(developer);
+
+}
+
+Developers.prototype.removeDeveloper=function(developer){
+  this.developers.pop(developer);
+
+}
+
+function Developer{
+  this.name;
+  this.email;
+  this.resume;
+
+}
+
+$(document).ready(function(){
+
+});
